8857f15689d668c0338a6fbc81489fcc11293677
Satyam Anand <satyam2001anand@gmail.com>
2023-07-17 02:39:16 +0530

    Attached front-End Part of Myshop

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * image controller
+ * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::image.image');
+module.exports = createCoreController('api::order.order');
